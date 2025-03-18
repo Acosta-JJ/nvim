03a15c5ea2cad6f2f6908db8a57d0dc97091b762
Jose Acosta <acostasantana.jj@gmail.com>
2025-03-18 16:33:36 +0000

    feat: remaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -10,7 +10,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "yamlls", "gopls", "terraformls", "nil_ls" }
+        ensure_installed = { "lua_ls","yamlls", "terraformls" }
       })
     end
   },
@@ -19,12 +19,10 @@ return {
     lazy = false,
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({})
       lspconfig.lua_ls.setup({})
       lspconfig.yamlls.setup({})
       lspconfig.gopls.setup({})
       lspconfig.terraformls.setup({})
-      lspconfig.nil_ls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', '<leader>gv', vim.lsp.buf.definition, {})
